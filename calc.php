db9260d60ba90b0e49e6c7af6e09a6ed529a5667
Kovacs Dorina <kovacsdorina2@gmail.com>
2022-05-24 09:17:29 +0200

    mukodik

--- calc.php
+++ calc.php
@@ -1,1 +1,27 @@
 <?php
+
+echo file_get_contents('templates/head.html');
+
+$page = file_get_contents('templates/calc.html');
+
+$aSide = $_GET['aSide'];
+$bSide = $_GET['bSide'];
+$cSide = $_GET['cSide'];
+$dSide = $_GET['dSide'];
+
+function calcArea($aSide, $bSide, $cSide, $dSide){
+$s = ($aSide+$bSide+$cSide+$dSide)/2;
+$area = sqrt(
+    ($s-$aSide)*
+    ($s-$bSide)*
+    ($s-$cSide)*
+    ($s-$dSide)
+);
+return $area;
+}
+$area = calcArea($aSide, $bSide, $cSide, $dSide);
+
+$page = str_replace('{{ result }}' , $area, $page);
+
+echo $page;
+echo file_get_contents('templates/foot.html');
