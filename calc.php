e986378a4e52b60545b40476b51dc16ac9330ed2
Kovacs Dorina <kovacsdorina2@gmail.com>
2022-05-24 09:48:41 +0200

    ervenyesseg vizsgalva

--- calc.php
+++ calc.php
@@ -4,11 +4,6 @@ echo file_get_contents('templates/head.html');
 
 $page = file_get_contents('templates/calc.html');
 
-$aSide = $_GET['aSide'];
-$bSide = $_GET['bSide'];
-$cSide = $_GET['cSide'];
-$dSide = $_GET['dSide'];
-
 function calcArea($aSide, $bSide, $cSide, $dSide){
 $s = ($aSide+$bSide+$cSide+$dSide)/2;
 $area = sqrt(
@@ -19,6 +14,24 @@ $area = sqrt(
 );
 return $area;
 }
+
+if(
+    !empty($_GET['aSide']) and
+    !empty($_GET['bSide']) and
+    !empty($_GET['cSide']) and
+    !empty($_GET['dSide'])
+    ){
+    
+    $aSide = $_GET['aSide'];
+    $bSide = $_GET['bSide'];
+    $cSide = $_GET['cSide'];
+    $dSide = $_GET['dSide'];
+    $area = calcArea($aSide, $bSide, $cSide, $dSide);
+    }
+    else{
+        $area = "Hiba! Helytelen bemenő adatok!";
+    }
+
 $area = calcArea($aSide, $bSide, $cSide, $dSide);
 
 $page = str_replace('{{ result }}' , $area, $page);
